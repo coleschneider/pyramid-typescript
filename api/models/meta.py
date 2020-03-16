988ab6b65a1fb80473ad3927187008393d6a35b8
Cole Schneider <colekschneider@gmail.com>
2020-03-16 00:00:39 -0700

    add json serializer to base class to serialize model on request

--- api/models/meta.py
+++ api/models/meta.py
@@ -13,4 +13,13 @@ NAMING_CONVENTION = {
 }
 
 metadata = MetaData(naming_convention=NAMING_CONVENTION)
-Base = declarative_base(metadata=metadata)
+class JsonBase(object):
+    def __json__(self, request):
+        json_exclude = getattr(self, '__json_exclude__', set())
+        return {key: value for key, value in self.__dict__.items()
+                # Do not serialize 'private' attributes
+                # (SQLAlchemy-internal attributes are among those, too)
+                if not key.startswith('_')
+                and key not in json_exclude}
+
+Base = declarative_base(metadata=metadata, cls=JsonBase)
--- api/views/todos.py
+++ api/views/todos.py
@@ -1,20 +1,32 @@
+
 from pyramid.view import view_config
+from pyramid.response import Response
 
-# class Todo:
-#     def __init__(self, id, body, complete):
-#         self.id = id
-#         self.body = body
-#         self.complete = complete
-#     def __json__(self, request):
-#         return {'id':self.id, 'body': self.body, 'complete': self.complete, }
+from sqlalchemy.exc import DBAPIError
 
-
+from .. import models
 
 
 @view_config(route_name='todos', renderer='json')
 def get_todos(request):
     """Sends back a list of todos"""
-    response = request.response
-    response.headers.extend({'Content-Type': 'application/json'})
-    
-    return [{'message': 1}]
+    try:
+        one = request.dbsession.query(models.Todo)
+    except DBAPIError:
+        return Response(db_err_msg, content_type='text/plain', status=500)
+    return list(one)
+
+db_err_msg = """\
+Pyramid is having a problem using your SQL database.  The problem
+might be caused by one of the following things:
+
+1.  You may need to initialize your database tables with `alembic`.
+    Check your README.txt for descriptions and try to run it.
+
+2.  Your database server may not be running.  Check that the
+    database server referred to by the "sqlalchemy.url" setting in
+    your "development.ini" file is running.
+
+After you fix the problem, please restart the Pyramid application to
+try it again.
+"""
